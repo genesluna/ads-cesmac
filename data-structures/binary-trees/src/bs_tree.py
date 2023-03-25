411513c9bd89525e95f419abc5d413779ea2c6dd
genesluna <genesluna@gmail.com>
2023-03-25 11:34:45 -0300

    added delete node feature

--- data-structures/binary-trees/src/bs_tree.py
+++ data-structures/binary-trees/src/bs_tree.py
@@ -21,3 +21,34 @@ class BSTree:
             node.right = self.__insert(node.right, value)
 
         return node
+
+    def delete(self, value):
+        self.root = self.__delete(self.root, value)
+
+    def __delete(self, node, value):
+        if node is None:
+            return node
+
+        if value < node.value:
+            node.left = self.__delete(node.left, value)
+        elif value > node.value:
+            node.right = self.__delete(node.right, value)
+        else:
+            if node.left is None:
+                return node.right
+            elif node.right is None:
+                return node.left
+            else:
+                temp = self.__get_min_value_node(node.right)
+                node.value = temp.value
+                node.right = self.__delete(node.right, temp.value)
+
+        return node
+
+    def __get_min_value_node(self, node):
+        current = node
+
+        while current.left is not None:
+            current = current.left
+
+        return current
